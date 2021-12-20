6cd77225a92a4d3b5da3743dc61e6c3f6f642735
VladGershun <VladYGershun@Yahoo.com>
2021-12-19 19:44:46 -0800

    Initial Commit

---
+++ AlgoViz/AlgoVizApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlgoVizApp.swift
+//  AlgoViz
+//
+//  Created by Vlad Gershun on 12/19/21.
+//
+
+import SwiftUI
+
+@main
+struct AlgoVizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
